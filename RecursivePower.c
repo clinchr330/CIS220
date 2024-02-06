b3bc613c748fa2dfa40f8f28c9d8e2fa524326b8
clinchr330 <clinchr330@students.cochise.edu>
2024-02-05 21:16:04 -0700

    Chpt2 PA

---
+++ RecursivePower.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+// Recursive function to calculate power
+int power(int base, int exponent) {
+    if (exponent == 1) {
+        return base;
+    } else {
+        return base * power(base, exponent - 1);
+    }
+}
+
+int main() {
+    int base, exponent, result;
+
+    printf("Enter base: ");
+    scanf("%d", &base);
+
+    printf("Enter exponent: ");
+    scanf("%d", &exponent);
+
+    result = power(base, exponent);
+
+    printf("%d^%d = %d\n", base, exponent, result);
+
+    return 0;
+}
